515da8359f51b755bcbf240350571c6e5de052f5
dan-mutua <daniel.mutua@student.moringaschool.com>
2022-04-09 12:06:51 +0300

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn waridi.wsgi
--- waridi/settings.py
+++ waridi/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config,Csv
+from decouple import config
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +131,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
